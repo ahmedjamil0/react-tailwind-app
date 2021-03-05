b379f8490254e3936cbb687e3d9dad987ae6d5fc
Ahmed Jamil <ahmed.jamil@alphasquadinc.com>
2021-03-05 19:39:29 +0500

    added some styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 const App = () => {
-  return <div>
-    <h1 className="text-red-900 pt-16 text-8xl font-black font-serif">Hello, World</h1>
-    <h1 className="text-red-900 pt-16 text-8xl font-black">Hello, World</h1>
-  </div>;
+  return <div className="p-6">
+    <h1 className="text-yellow-500  text-8xl font-bold ">Hello, World</h1>
+    <p className="text-gray-700 text-2xl font-normal mt-3"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, distinctio. </p>
+    <ul className="flex mt-2">
+      <li className="px-2">Home</li>
+      <li className="px-2">Contact</li>
+    </ul>
+      <h2 className="text-gray-700 text-4xl">Home</h2>
+      <p className="text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia impedit ab ad sequi ipsum consequuntur inventore laboriosam ipsam quas molestias.</p>
+ <h2 className="text-gray-700 text-4xl">Contact</h2>
+     <form>
+       <div>
+       <label htmlFor="email" className="text-xl font-semibold text-gray-700">Email</label>
+       <input type="email" name="email" className="border" />
+
+       </div>
+       <div>
+
+       <label htmlFor="name" className="text-xl font-semibold text-gray-700">Name</label>
+       <input type="text" name="name" className="border" />
+       </div>
+       <button className="bg-yellow-600 px-5 py-2 mt-3 text-white">Send</button>
+     </form>
+</div>;
 };
 
 export default App;
